ff8eec58b64f3c6e13319bfa38249ef88a7d56a2
magaowei <osxingzhe@163.com>
2020-07-23 20:11:44 +0800

    两数之和

---
+++ src/main/java/pub/ants/middle/question2/ListNode.java
@@ -0,0 +1,17 @@
+package pub.ants.middle.question2;
+
+/**
+ * @author: magaowei
+ * @Date: 2020/7/23 19:10
+ * @Description:
+ *   listNode链表结构
+ */
+public class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+  }
+}
--- src/main/java/pub/ants/middle/question2/Solution.java
+++ src/main/java/pub/ants/middle/question2/Solution.java
@@ -62,12 +62,3 @@ public class Solution {
   }
 }
 
-class ListNode {
-
-  int val;
-  ListNode next;
-
-  ListNode(int x) {
-    val = x;
-  }
-}
